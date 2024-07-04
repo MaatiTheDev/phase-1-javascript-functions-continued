67964affc008d4f3249e31f8c726612b70109de2
felix maati <maatifelix001@gmail.com>
2024-07-05 00:16:41 +0300

    short message

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+//  Define the saturdayFun function
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  //  Define the mondayWork function using a function expression
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // Define the wrapAdjective function
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  console.log(saturdayFun()); // "This Saturday, I want to roller-skate!"
+  console.log(saturdayFun("swim")); // "This Saturday, I want to swim!"
+  
+  console.log(mondayWork()); // "This Monday, I will go to the office."
+  console.log(mondayWork("work from home")); // "This Monday, I will work from home."
+  
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("a dedicated programmer")); // "You are !!!a dedicated programmer!!!"
+  
+  console.log(wrapAdjective("%")("a dedicated programmer")); // "You are %a dedicated programmer%!"
+  
